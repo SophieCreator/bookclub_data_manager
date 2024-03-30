4918c64efdef4bd533faadc874e8a52c7654f586
mamou <galateya.cheba@gmail.com>
2024-03-30 20:24:12 +0300

    ...error...

--- src/main/java/com/bookclub_data_manager/repository/MyQuestionRepository.java
+++ src/main/java/com/bookclub_data_manager/repository/MyQuestionRepository.java
@@ -4,13 +4,14 @@ import com.bookclub_data_manager.models.MyQuestion;
 import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
-public interface MyQuestionRepository {
+public interface MyQuestionRepository extends CrudRepository<MyQuestion, Integer> {
 
     @Query(value = "SELECT * FROM questions WHERE question_id = :question_id", nativeQuery = true)
     MyQuestion getQuestionById(@Param("question_id") int question_id);
--- src/main/java/com/bookclub_data_manager/repository/TestRepository.java
+++ src/main/java/com/bookclub_data_manager/repository/TestRepository.java
@@ -1,16 +1,18 @@
 package com.bookclub_data_manager.repository;
 
+import com.bookclub_data_manager.models.Meeting;
 import com.bookclub_data_manager.models.Test;
 import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
-public interface TestRepository {
+public interface TestRepository extends CrudRepository<Test, Integer> {
 
     @Query(value = "SELECT * FROM tests WHERE test_id = :test_id", nativeQuery = true)
     Test getTestById(@Param("test_id") int test_id);
